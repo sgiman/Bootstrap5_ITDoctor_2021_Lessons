34e39469a3aa380e08081bbde9ac7574053a7c07
Sergey Gasanov <sgimancs@gmail.com>
2023-09-24 16:43:18 +0300

    test
    test

---
+++ 001_Download and connection/js/main.js
@@ -0,0 +1,21 @@
+/****************************************************************************************
+ * 001_Обзор, загрузка и подключение
+ * --------------------------------------------------------------------------------------
+ * Visual Studio Code
+ * Bootstrap 5.3.2 (without jQuery)
+ * HTML, CSS, JavaScript
+ *
+ * Bootstrap 5 docs		
+ * https://getbootstrap.com/docs/5.0/getting-started/introduction/
+ *	
+ * bootstrap 5 & font awesome snippets 
+ * https://marketplace.visualstudio.com/items?itemName=HansUXdev.bootstrap5-snippets
+ *
+ * --------------------------------------------------------------------------------------
+ * Bootstrap 5 уроки для начинающих
+ * ITDoctor (2021)
+ * https://www.youtube.com/playlist?list=PLuY6eeDuleINmJ4DV22gtPL4--HQ49Df3
+ *
+ ****************************************************************************************
+ * Writing by sgiman @ 2023-09
+ */
---
+++ 004_Grid 2x2/site/js/main.js
@@ -0,0 +1,24 @@
+/****************************************************************************************
+ * 004_Сетка 2 на 2
+ * --------------------------------------------------------------------------------------
+ * Visual Studio Code
+ * Bootstrap 5.3.2 (without jQuery)
+ * HTML, CSS, JavaScript
+ *
+ * Bootstrap 5 docs		
+ * https://getbootstrap.com/docs/5.0/getting-started/introduction/
+ *
+ * Bootstrap 5 & font awesome snippets (Visual Studio Code)
+ * https://marketplace.visualstudio.com/items?itemName=HansUXdev.bootstrap5-snippets
+ *
+ * Fontello - icon fonts generator
+ * https://fontello.com/
+ *
+ * --------------------------------------------------------------------------------------
+ * Bootstrap 5 уроки для начинающих
+ * ITDoctor (2021)
+ * https://www.youtube.com/playlist?list=PLuY6eeDuleINmJ4DV22gtPL4--HQ49Df3
+ *
+ ****************************************************************************************
+ * Writing by sgiman (SCS) @ 2023-09
+ */
