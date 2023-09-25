4df11dbc0793c7bbe051759117be9b3ef2ea63fe
Sergey Gasanov <sgimancs@gmail.com>
2023-09-25 08:54:31 +0300

    update
    update

--- 005_Accordion/site/js/main.js
+++ 005_Accordion/site/js/main.js
@@ -5,6 +5,10 @@
  * Bootstrap 5.3.2 (without jQuery)
  * HTML, CSS, JavaScript
  *
+ * ORIGINAL:
+ * https://morphismail.github.io/video_course_basic/
+ * 
+ * --------------------------------------------------------------------------------------- * 
  * Bootstrap 5 docs		
  * https://getbootstrap.com/docs/5.0/getting-started/introduction/
  *
@@ -14,11 +18,35 @@
  * Fontello - icon fonts generator
  * https://fontello.com/
  *
+ * Bootstrap 5 & font awesome snippets (Visual Studio Code)
+ * https://marketplace.visualstudio.com/items?itemName=HansUXdev.bootstrap5-snippets
+ *
+ * Gradient Constructor
+ * https://www.colorzilla.com/gradient-editor/
+ *
+ * CSS triangle generator
+ * http://apps.eky.hk/css-triangle-generator/
+ *
+ * Embed Responsively (for youtube url)
+ * https://embedresponsively.com/
+ * 
  * --------------------------------------------------------------------------------------
  * Bootstrap 5 уроки для начинающих
+ * Bootstrap 5 tutorials for beginners
  * ITDoctor (2021)
  * https://www.youtube.com/playlist?list=PLuY6eeDuleINmJ4DV22gtPL4--HQ49Df3
  *
- ****************************************************************************************
- * Writing by sgiman (SCS) @ 2023-09
+ *----------------------------------------------------------------------------------------
+ * VS CODE:
+ * 
+ * ALT+SHIFT+F - авто-выравнивание СSS-кода 
+ * ALT+D - дублирование
+ * ALT-Z - перенос строки 
+ * 
+ * p20-auto-20 - padding
+ * m30-auto - margin
+ * lorem50 - шаблонный текст
+ * 
+ * ****************************************************************************************
+ * Writing & Modification by sgiman (SCS) @ 2023-09
  */
